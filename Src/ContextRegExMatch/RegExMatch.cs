4d450658318867b68e195542060a9b5c9e5a255d
riha <richard.hallgren@gmail.com>
2014-12-05 11:59:16 +0100

    Cleaning up some unused code

--- Src/ContextRegExMatch/RegExMatch.cs
+++ Src/ContextRegExMatch/RegExMatch.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.ComponentModel;
-using System.IO;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using BizTalkComponents.Utils;
 using Microsoft.BizTalk.Component.Interop;
 using Microsoft.BizTalk.Message.Interop;
-using Microsoft.BizTalk.Streaming;
 
 namespace BizTalkComponents.ContextRegExMatch
 {
@@ -61,8 +59,6 @@ namespace BizTalkComponents.ContextRegExMatch
             var contextNamespaceToSetProperty = new ContextProperty(ContextNamespaceToSet);
             var contextNamespaceToMatchProperty = new ContextProperty(ContextNamespaceToMatch);
 
-            pInMsg.BodyPart.Data = ReadStreamToEndAndSeekToBeginning(pInMsg.BodyPart.Data, true, 1048576);
-
             var regex = new Regex(PatternToMatch);
 
             string value;
@@ -75,21 +71,6 @@ namespace BizTalkComponents.ContextRegExMatch
                 pInMsg.Context.Promote(contextNamespaceToSetProperty.PropertyName, contextNamespaceToSetProperty.PropertyNamespace, ValueToSet);
 
             return pInMsg;
-        }
-
-        Stream ReadStreamToEndAndSeekToBeginning(Stream data, bool seekToBeginning, int bufferSize)
-        {
-            var buffer = new byte[bufferSize];
-            Stream outputStream = data;
-
-            outputStream = new ReadOnlySeekableStream(outputStream);
-
-            while (0 != outputStream.Read(buffer, 0, buffer.Length)) { }
-
-            if (seekToBeginning)
-                outputStream.Seek(0, SeekOrigin.Begin);
-
-            return outputStream;
         }
     }
 }
