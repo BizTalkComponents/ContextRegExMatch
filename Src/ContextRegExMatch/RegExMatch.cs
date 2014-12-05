356962803958c403542b3c1827e6180a19dab98a
riha <richard.hallgren@gmail.com>
2014-12-05 10:43:55 +0100

    Updating NuGet packages

--- Src/ContextRegExMatch/RegExMatch.cs
+++ Src/ContextRegExMatch/RegExMatch.cs
@@ -11,72 +11,53 @@ using Microsoft.BizTalk.Streaming;
 namespace BizTalkComponents.ContextRegExMatch
 {
     [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
-    [ComponentCategory(CategoryTypes.CATID_Any)]
+    [ComponentCategory(CategoryTypes.CATID_Receiver)]
     [Guid("E0105181-E664-4770-B20B-6EB5AF77DBA0")]
 
     public partial class RegExMatchComponent : IBaseComponent, Microsoft.BizTalk.Component.Interop.IComponent, IComponentUI, IPersistPropertyBag
     {
+        [RequiredRuntime]
         [DisplayName("Pattern To Match")]
         [Description("The RegEx pattern used to match the specific context value against.")]
         public string PatternToMatch { get; set; }
-        private const string PatternToMatchPropertyName = "PatternToMatch";
+        private const string PatternToMatchPropertyId = "PatternToMatch";
 
+        [RequiredRuntime]
         [DisplayName("Context Property To Match")]
         [Description("Namespace and value of the context property that the match should execute against. Should be in format 'http://foo.bar#value'.")]
         public string ContextNamespaceToMatch { get; set; }
-        private const string ContextNamespaceToMatchPropertyName = "ContextNamespaceToMatch";
+        private const string ContextNamespaceToMatchPropertyId = "ContextNamespaceToMatch";
 
+        [RequiredRuntime]
         [DisplayName("Context Property To Set")]
         [Description("Namespace and value of the context property that should be used to the set the value. Should be in format 'http://foo.bar#value'.")]
         public string ContextNamespaceToSet { get; set; }
-        private const string ContextNamespaceToSetPropertyName = "ContextNamespaceToSet";
+        private const string ContextNamespaceToSetPropertyId = "ContextNamespaceToSet";
 
+        [RequiredRuntime]
         [DisplayName("Value To Set")]
         [Description("The value to set if the match i successful")]
         public string ValueToSet { get; set; }
-        private const string ValueToSetPropertyName = "ValueToSet";
-        
+        private const string ValueToSetPropertyId = "ValueToSet";
+
         public void Load(IPropertyBag propertyBag, int errorLog)
         {
-            if (string.IsNullOrEmpty(PatternToMatch))
-                PatternToMatch = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, PatternToMatchPropertyName), string.Empty);
-            
-
-            if (string.IsNullOrEmpty(ValueToSet))
-                ValueToSet = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, ValueToSetPropertyName), string.Empty);
-            
-
-            if (string.IsNullOrEmpty(ContextNamespaceToMatch))
-                ContextNamespaceToMatch = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, ContextNamespaceToMatchPropertyName), string.Empty);
-            
-
-            if (string.IsNullOrEmpty(ContextNamespaceToSet))
-                ContextNamespaceToSet = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, ContextNamespaceToMatchPropertyName), string.Empty);
-            
+            PatternToMatch = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, PatternToMatchPropertyId), string.Empty);
+            ValueToSet = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, ValueToSetPropertyId), string.Empty);
+            ContextNamespaceToMatch = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, ContextNamespaceToMatchPropertyId), string.Empty);
+            ContextNamespaceToSet = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, ContextNamespaceToMatchPropertyId), string.Empty);
         }
 
         public void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
         {
-            PropertyBagHelper.WritePropertyBag(propertyBag, PatternToMatchPropertyName, PatternToMatch);
-            PropertyBagHelper.WritePropertyBag(propertyBag, ValueToSetPropertyName, ValueToSet);
-            PropertyBagHelper.WritePropertyBag(propertyBag, ContextNamespaceToMatchPropertyName, ContextNamespaceToMatch);
-            PropertyBagHelper.WritePropertyBag(propertyBag, ContextNamespaceToSetPropertyName, ContextNamespaceToSet);
+            PropertyBagHelper.WritePropertyBag(propertyBag, PatternToMatchPropertyId, PatternToMatch);
+            PropertyBagHelper.WritePropertyBag(propertyBag, ValueToSetPropertyId, ValueToSet);
+            PropertyBagHelper.WritePropertyBag(propertyBag, ContextNamespaceToMatchPropertyId, ContextNamespaceToMatch);
+            PropertyBagHelper.WritePropertyBag(propertyBag, ContextNamespaceToSetPropertyId, ContextNamespaceToSet);
         }
 
         public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
         {
-            if(string.IsNullOrEmpty(PatternToMatch))
-                throw new ArgumentException("Pattern property to use for RegEx match cannot be null");
-
-            if (string.IsNullOrEmpty(ContextNamespaceToMatch))
-                throw new ArgumentException("Context property to match cannot be null");
-
-            if (string.IsNullOrEmpty(ContextNamespaceToSet))
-                throw new ArgumentException("Context property to set cannot be null");
-            
-            if (string.IsNullOrEmpty(ValueToSet))
-                throw new ArgumentException("Value to set cannot be null");
-
             var contextNamespaceToSetProperty = new ContextProperty(ContextNamespaceToSet);
             var contextNamespaceToMatchProperty = new ContextProperty(ContextNamespaceToMatch);
 
@@ -88,7 +69,7 @@ namespace BizTalkComponents.ContextRegExMatch
             var result = pInMsg.Context.TryRead(contextNamespaceToMatchProperty, out value);
 
             if (!result)
-                throw new ArgumentException("Context property to use for RegEx match can be null");
+                throw new ArgumentException("Context property value to use for RegEx match can be null");
 
             if (regex.IsMatch(value))
                 pInMsg.Context.Promote(contextNamespaceToSetProperty.PropertyName, contextNamespaceToSetProperty.PropertyNamespace, ValueToSet);
