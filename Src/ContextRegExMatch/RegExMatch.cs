e836ec446fa50cc9adb7e30dda06ba5cfe1f742f
riha <richard.hallgren@gmail.com>
2014-12-18 10:36:26 +0100

    Fixed a bug with previously set context values

--- Src/ContextRegExMatch/RegExMatch.cs
+++ Src/ContextRegExMatch/RegExMatch.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.ComponentModel;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using BizTalkComponents.Utils;
 using Microsoft.BizTalk.Component.Interop;
 using Microsoft.BizTalk.Message.Interop;
+using Microsoft.BizTalk.Streaming;
 
 namespace BizTalkComponents.ContextRegExMatch
 {
@@ -34,7 +36,7 @@ namespace BizTalkComponents.ContextRegExMatch
 
         [RequiredRuntime]
         [DisplayName("Value To Set")]
-        [Description("The value to set if the match i successful")]
+        [Description("The value to set if the match is successful")]
         public string ValueToSet { get; set; }
         private const string ValueToSetPropertyId = "ValueToSet";
 
@@ -58,8 +60,12 @@ namespace BizTalkComponents.ContextRegExMatch
         {
             var contextNamespaceToSetProperty = new ContextProperty(ContextNamespaceToSet);
             var contextNamespaceToMatchProperty = new ContextProperty(ContextNamespaceToMatch);
-
             var regex = new Regex(PatternToMatch);
+
+            // This will force promotion of in the pipeline previously set property promotions. 
+            // This is needed as promotion will occur in a streaming fashion and we otherwise won't have the previously promoted property within 
+            // our current context.
+            pInMsg.BodyPart.Data = ReadStreamToEndAndSeekToBeginning(pInMsg.BodyPart.Data, true, 1048576);
 
             string value;
             var result = pInMsg.Context.TryRead(contextNamespaceToMatchProperty, out value);
@@ -71,6 +77,21 @@ namespace BizTalkComponents.ContextRegExMatch
                 pInMsg.Context.Promote(contextNamespaceToSetProperty.PropertyName, contextNamespaceToSetProperty.PropertyNamespace, ValueToSet);
 
             return pInMsg;
+        }
+
+        Stream ReadStreamToEndAndSeekToBeginning(Stream data, bool seekToBeginning, int bufferSize)
+        {
+            var buffer = new byte[bufferSize];
+            Stream outputStream = data;
+
+            outputStream = new ReadOnlySeekableStream(outputStream);
+
+            while (0 != outputStream.Read(buffer, 0, buffer.Length)) { }
+
+            if (seekToBeginning)
+                outputStream.Seek(0, SeekOrigin.Begin);
+
+            return outputStream;
         }
     }
 }
